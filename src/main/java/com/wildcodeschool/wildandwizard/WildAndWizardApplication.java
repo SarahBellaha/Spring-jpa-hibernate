a41c0f568c20c76fc366d30eeca12306ac1bf24a
BELLAHA Sarah (SNCF VOYAGEURS / Etablissement Gares Transilien Sud Ligne C / TERR JUINE ET ORGE) <9008080s@commun.ad.sncf.fr>
2022-11-15 16:10:46 +0100

    upload Spring-04

--- src/main/java/com/wildcodeschool/wildandwizard/WildAndWizardApplication.java
+++ src/main/java/com/wildcodeschool/wildandwizard/WildAndWizardApplication.java
@@ -8,6 +8,7 @@ public class WildAndWizardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WildAndWizardApplication.class, args);
+
 	}
 
 }
--- src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
+++ src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
@@ -5,10 +5,7 @@ import com.wildcodeschool.wildandwizard.repository.WizardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
